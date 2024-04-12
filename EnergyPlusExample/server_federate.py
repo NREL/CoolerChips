76c7506fbff47bea48b2198ec970caa6e13e5858
Jermy Thomas <jermythomasv@gmail.com>
2024-04-12 03:51:25 -0400

    updated server federate

--- EnergyPlusExample/server_federate.py
+++ EnergyPlusExample/server_federate.py
@@ -2,8 +2,9 @@
 
 import pandas as pd
 import federate
-import h5py
-from scipy.interpolate import interp1d
+import numpy as np
+from scipy.interpolate import Rbf
+from sklearn.preprocessing import MinMaxScaler
 import numpy as np
 import definitions
 import logging
@@ -13,16 +14,6 @@ logger = logging.getLogger(__name__)
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
-# Read modes and coefficients from CSV files outside the function
-Modes = pd.read_csv('ThermalModel_datacenter/Modes.csv')
-modes = Modes.to_numpy()
-
-Coeff = pd.read_csv('ThermalModel_datacenter/coeff.csv')
-coeff = Coeff.to_numpy()
-
-# Define the velocity range for which the coefficients are known
-vel = np.arange(6, 16)
-solution_path = "ThermalModel_datacenter/solution_PythonPOD_Solid.cgns"
 
 PUBS = [
     {
@@ -47,50 +38,163 @@ SUBS = [
 class Server_thermal_federate:
     def __init__(self) -> None:
         self.total_time = definitions.TOTAL_SECONDS  # get this from IDF
+        self.modes = np.loadtxt('ThermalModel_datacenter/Modes.csv', delimiter=',')
+        self.coefficients = np.loadtxt('ThermalModel_datacenter/coeff.csv', delimiter=',')
+        self.parameter_array=np.loadtxt('ThermalModel_datacenter/parameter_array.csv', delimiter=',')
+        
+        self.rbf_models, self.param_scaler, self.coeff_scaler = self.build_and_scale_rbf_models(self.parameter_array, self.coefficients, kernel_function='multiquadric')
+        
         self.subs = [federate.Sub(name=f'{sensor["variable_key"]}/{sensor["variable_name"]}', unit=sensor["variable_unit"]) for sensor in definitions.SENSORS]
         self.pubs = [federate.Pub(name=f'{pub["Name"]}', unit=pub["Units"]) for pub in PUBS]
         self.server_federate = federate.mostcool_federate(federate_name="Server_1", subscriptions=self.subs, publications=self.pubs)
         self.server_federate.time_interval_seconds = definitions.TIMESTEP_PERIOD_SECONDS
-           
-    def thermal_model(self, Ts, mass_flowrate, power_level=1, num_servers=42):
-        #TODO: Feed in Data Center CPU Loading Schedule to get power level
+    
+    # This fucntion "build_and_scale_rbf_models" has to be run once to run the online_prediction function multiple time inside Helics
+    def build_and_scale_rbf_models(self, 
+                                   training_parameters, 
+                                   training_coefficients, 
+                                   kernel_function='multiquadric'):
+        """
+        Build RBF models for each coefficient dimension with parameter and coefficient scaling.
+
+        Parameters:
+        - training_parameters: 2D array of parameters used for training.
+        - training_coefficients: 2D array of coefficients corresponding to the training parameters.
+        - kernel_function: String specifying the kernel function for the RBF models.
+
+        Returns:
+        - rbf_models: List of RBF model objects for each coefficient dimension.
+        - param_scaler: Fitted scaler for parameters.
+        - coeff_scaler: Fitted scaler for coefficients.
+        """
+        # Initialize and fit scalers
+        param_scaler = MinMaxScaler().fit(training_parameters)
+        coeff_scaler = MinMaxScaler().fit(training_coefficients)
+        
+        # Scale training data
+        scaled_training_params = param_scaler.transform(training_parameters)
+        scaled_training_coeffs = coeff_scaler.transform(training_coefficients)
+
+        # Build RBF models with scaled data
+        rbf_models = [
+            Rbf(scaled_training_params[:, 0], scaled_training_params[:, 1], scaled_training_coeffs[:, i], function=kernel_function)
+            for i in range(scaled_training_coeffs.shape[1])
+        ]
+        print("RBF models built successfully")
+
+        return rbf_models, param_scaler, coeff_scaler
+    
+    # Main function to be passed to Helics
+    def online_prediction(self, velocity, CPU_load_fraction, inlet_server_temperature,num_servers, rbf_models, param_scaler, coeff_scaler, pod_modes):
+        """
+        Predict the system state for new velocity, heat load fraction, and inlet_server_temperature using the 
+        Reduced Order Model developed in the offline stage. Adjust the predicted temperature state 
+        based on the deviation from the assumed inlet temperature of 30°C.
+
+        Parameters:
+        - velocity: New velocity value for which to predict the system state.
+        - heat_load_fraction: New heat load fraction value for which to predict the system state.
+        - inlet_server_temperature: inlet temperature from the assumed 30°C. This should be in °C
+        - rbf_models: List of pre-built RBF model objects for predicting each coefficient dimension.
+        - param_scaler: Scaler object used for parameter normalization, obtained from the offline stage.
+        - coeff_scaler: Scaler object used for coefficients normalization, obtained from the offline stage.
+        - pod_modes: 2D array of POD modes obtained from the offline stage.
+
+        Returns:
+        - adjusted_predicted_state: 2D array of the predicted system state for the new parameters,
+                                    adjusted for the inlet temperature deviation.
+        """
+        
+
+        
+        # Create a 2D array from the input parameters
+        new_parameters = np.array([[velocity, CPU_load_fraction]])
+        
+        # Normalize the new parameters using the parameter scaler
+        normalized_new_params = param_scaler.transform(new_parameters)
+        
+        # Use the RBF models to predict the normalized coefficients for the new parameters
+        predicted_normalized_coeffs = np.array([
+            model(normalized_new_params[:, 0], normalized_new_params[:, 1]) for model in rbf_models
+        ]).T  # Transpose to ensure correct shape
+        
+        # Invert the normalization of the coefficients
+        predicted_coeffs = coeff_scaler.inverse_transform(predicted_normalized_coeffs)
+        
+        # Reconstruct the system state using the predicted coefficients and POD modes
+        #The predicted state is in K
+        predicted_state = np.dot(pod_modes, predicted_coeffs.T) 
+        #Conveting the Tthe predicted temperatures from K to °C
+        predicted_state = predicted_state-273.15
+        
+        # Adjust the predicted state by the temperature deviation
+        # Assuming that the temperature is the quantity of interest in the predicted state
+        adjusted_predicted_state = predicted_state - (30-inlet_server_temperature)
+        #The above assumption only hold true when constant fluid properties are assumed i.e., the inherent assumption is that fluid properties
+        # will not change significantly with changing temperature. 
+        #Extrating the maximum CPU temperature from the array
+        CPU_temp_max=np.max(adjusted_predicted_state)
+
         # Define constants
         dens = 1.225 #kg/m^3
         cp = 1006.43 #J/kgK
-
-        # Adjust mass_flowrate based on power level
-        base_mass_flowrate = 0.322665000000000 #kg/s
+        server_inlet_area= 0.017560001 # in m^2
+        mass_flowrate=dens*server_inlet_area*velocity
+        # Load per CPU = 300 W and 2 CPU's are present in the server making total CPU laod to be 600W at 100% capcity
+        # All other components RAM, power supply and HDD etc amount to 400W making the total server load to be 1000 W
         if mass_flowrate == 0:
-            mass_flowrate = base_mass_flowrate * power_level #power level in kW
-        
-        # Calculate pressure drop
-        pressure_drop = 7.3794e+03 * mass_flowrate ** 1.9087 #Pa
-        
-        # Calculate temperatures and energy consumption based on power level
-        if power_level == 1:
-            del_T1 = 10
-            del_T2 = -5
-        elif power_level == 0.8:
-            del_T1 = 7.5
-            del_T2 = -3
-        elif power_level == 0.6:
-            del_T1 = 5
-            del_T2 = -1.5
-        else:
-            # Default values if power_level is not recognized
-            del_T1 = del_T2 = 0
-        
-        # Calculate server temperatures
-        T_in_server = Ts + del_T1
-        # T_out_server = T_in_server + power_level * 1000 / (mass_flowrate * cp)
-        
-        # Calculate energy consumption by fans
-        energy_consumption_per_fan = pressure_drop * mass_flowrate / dens
-        energy_consumption = energy_consumption_per_fan * num_servers
+            mass_flowrate = 0.00001
+        T_out_server = inlet_server_temperature + (CPU_load_fraction * 600+400)/ (mass_flowrate * cp)
 
-        # Return the supply approach temperature, return approach temperature, and energy consumed by fans
-        return del_T1, del_T2, energy_consumption
+        return CPU_temp_max, T_out_server
+
+
     
+    def data_center_temperature_deltas(self, 
+                                       supply_temperature,
+                                       server_inlet_velocity,
+                                       total_ite_load_percentage,
+                                       num_servers):
+        """
+        Calculate various temperature deltas based on the total ITE load.
+
+        Parameters:
+        - total_ite_load_percentage: The total ITE load %
+
+        Returns:
+        - calculated temperature deltas and other values.
+        The average supply and return delta_T's should be used as inputs for data_center
+        The max supply delta_T can should be used as input to server model 
+        """
+        Total_ITE_Load=80 #in KW
+        ite_load = Total_ITE_Load * total_ite_load_percentage
+        avg_supply_delta_T = 0.1169 * ite_load**0.9505
+        avg_return_delta_T = 7.6459e-04 * ite_load**1.8956
+        max_supply_del_T = 0.0173 * ite_load**1.5363 + 6.9092
+        max_return_del_T = -4.7537 * np.exp(ite_load * -0.0099)
+
+        inlet_server_temperature=supply_temperature+max_supply_del_T #Worst case scenario for the server, i.e., hihgest inlet temperature at the server inlet
+        #print(inlet_server_temperature)
+        # Define constants
+        dens = 1.225 #kg/m^3
+        cp = 1006.43 #J/kgK
+        server_inlet_area= 0.017560001 # in m^2
+        mass_flowrate=dens*server_inlet_area*server_inlet_velocity
+        # Calculation of pressure drop across the air cooled server
+        pressure_drop = 7.4066e+03 * mass_flowrate ** 1.8384 #Pa
+        # Calculate energy consumption by fans to cool the CPU
+        energy_consumption_by_fans_per_server = pressure_drop * mass_flowrate / dens #in Watts
+        total_energy_consumption_by_fans = energy_consumption_by_fans_per_server * num_servers #in Watts
+        # Load per CPU = 300 W and 2 CPU's are present in the server making total CPU laod to be 600W at 100% capcity
+        # All other components RAM, power supply and HDD etc amount to 400W making the total server load to be 1000 W
+        Total_server_Load=ite_load*1000-total_energy_consumption_by_fans
+        heat_load_per_server=Total_server_Load/num_servers
+        CPU_Load=heat_load_per_server-400 #in Watts
+        CPU_Load_fraction=CPU_Load/600 # is a fraction of the total CPU load of 600W
+        #print(CPU_Load_fraction)
+
+        return avg_supply_delta_T,avg_return_delta_T, inlet_server_temperature,CPU_Load_fraction
+
     def run(self):
         while self.server_federate.granted_time < self.total_time:
             self.server_federate.update_subs()
@@ -104,10 +208,26 @@ class Server_thermal_federate:
                 elif sub.name == "Data Center CPU Loading Schedule/Schedule Value":
                     cpu_loading = sub.value
             print(f"Ts: {Ts}, mass_flow_rate: {mass_flow_rate}, cpu_loading: {cpu_loading}  at time {self.server_federate.granted_time}")
-            supply_approach_temp, return_approach_temperature, energy_fan = self.thermal_model(Ts=Ts, 
-                                                                                               mass_flowrate=mass_flow_rate, 
-                                                                                               power_level=cpu_loading, 
-                                                                                               num_servers=42)
+            # supply_approach_temp, return_approach_temperature, energy_fan = self.thermal_model(Ts=Ts, 
+            #                                                                                    mass_flowrate=mass_flow_rate, 
+            #                                                                                    power_level=cpu_loading, 
+            #                                                                                    num_servers=42)
+            num_servers= 84
+            server_inlet_velocity=5#in m/s
+            supply_approach_temp, return_approach_temperature, inlet_server_temperature, CPU_Load_fraction = self.data_center_temperature_deltas(supply_temperature=Ts, 
+                                                                                                                                            server_inlet_velocity=server_inlet_velocity, 
+                                                                                                                                            total_ite_load_percentage=cpu_loading, 
+                                                                                                                                            num_servers=num_servers)
+            CPU_temp_max, T_out_server = self.online_prediction(server_inlet_velocity, 
+                                                           CPU_Load_fraction, 
+                                                           inlet_server_temperature,
+                                                           num_servers, 
+                                                           self.rbf_models, 
+                                                           self.param_scaler, 
+                                                           self.coeff_scaler, 
+                                                           self.modes)
+            
+            
             if supply_approach_temp is not None:
                 self.pubs[0].value = supply_approach_temp
             if return_approach_temperature is not None:
